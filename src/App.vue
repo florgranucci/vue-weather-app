85999e514e883910c53e9218e609bed6cace18fd
florgranucci <flor.g.24@outlook.com>
2021-04-04 17:58:53 -0300

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,194 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" :class="typeof weather.main != 'undefined' && weather.main.temp > 16 ? 'warm' : ''">
+    <main>
+      <div class="search-box">
+        <input
+          type="text"
+          class="search-bar"
+          placeholder="Search..."
+          v-model="query"
+          @keypress="getWeather"
+        />
+      </div>
+      <div class="weather-wrap"  v-if= "typeof weather.main != 'undefined'" >
+        <div class="location-box">
+          <div class="location">{{ weather.name }}, {{ weather.sys.country }}</div>
+          <div class="date">{{ handleDate() }}</div>
+        </div>
+
+        <div class="weather-box">
+          <div class="temperature"> {{ Math.round(weather.main.temp) }}ºc </div>
+          <div class="weather">{{ weather.weather[0].description }}</div>
+          <ul class='list'>
+            <li>Feels like: {{ weather.main.feels_like }}ºc</li>
+            <li>Min: {{ weather.main.temp_min }}ºc</li>
+            <li>Max: {{ weather.main.temp_max }}ºc</li>
+          </ul>
+        </div>
+      </div>
+    </main>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import axios from 'axios';
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  data() {
+    return {
+      api_key: "07dca15171d031632e0525d4def857e4",
+      url_base: "https://api.openweathermap.org/data/2.5/",
+      query: "",
+      weather: {},
+    };
+  },
+  methods: {
+    getWeather(e) {
+      if (e.key == "Enter") {
+        axios.get(`${this.url_base}weather?q=${this.query}&units=metric&APPID=${this.api_key}`)
+          .then((res) => res.data)
+          .then(data => {
+            if(data.main !== undefined){
+              this.saveResults(data)
+            }
+          });
+      }
+    },
+
+    saveResults(results) {
+      this.weather = results;
+      console.log(this.weather)
+    },
+
+    handleDate () {
+      let d = new Date();
+      let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+      let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+      let day = days[d.getDay()];
+      let date = d.getDate();
+      let month = months[d.getMonth()];
+      let year = d.getFullYear();
+
+      return `${day} ${date} ${month} ${year}`
+    }
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
 }
+
+body {
+  font-family: "monserrat", sans-serif;
+}
+
+#app {
+  background-image: url("./assets/cold-bg.jpg");
+  background-size: cover;
+  background-position: bottom;
+  transition: 0.4s;
+}
+
+#app.warm {
+  background-image: url('./assets/sunny-bg.jpg');
+}
+
+
+main {
+  min-height: 100vh;
+  padding: 25px;
+  background-image: linear-gradient(
+    to bottom,
+    rgba(0, 0, 0, 0.25),
+    rgba(0, 0, 0, 0.75)
+  );
+}
+
+.search-box {
+  width: 100%;
+  margin-bottom: 30px;
+}
+
+.search-box .search-bar {
+  display: block;
+  width: 100%;
+  padding: 15px;
+  margin-top: 50px;
+  margin-bottom: 50px;
+  color: "#313131";
+  font-size: 20px;
+  appearance: none;
+  border: none;
+  outline: none;
+  background: none;
+  box-shadow: 0px 0px 8px rgba(0, 0, 0, 0.15);
+  background-color: rgba(255, 255, 255, 0.25);
+  border-radius: 0px 16px 0px 16px;
+  transition: 0.4s;
+}
+
+.search-box .search-bar:focus {
+  box-shadow: 0px 0px 16px rgba(0, 0, 0, 0.15);
+  background-color: rgba(255, 255, 255, 0.75);
+  border-radius: 16px 0px 16px 0px;
+}
+
+.location-box .location {
+  color: #fff;
+  font-size: 32px;
+  font-weight: 500;
+  text-align: center;
+  text-shadow: 1px 3px rgba(0, 0, 0, 0.25);
+}
+
+.location-box .date {
+  color: #fff;
+  font-size: 18px;
+  font-weight: 300;
+  text-align: center;
+  font-style: italic;
+}
+
+.weather-box {
+  text-align: center;
+}
+
+.weather-box .temperature {
+  display: inline-block;
+  padding: 10px 25px;
+  color: #fff;
+  font-size: 102px;
+  font-weight: 900;
+  text-shadow: 3px 6px rgba(0, 0, 0, 0.25);
+  background-color: rgba(255, 255, 255, 0.25);
+  margin: 30px 0px;
+  border-radius: 16px;
+  box-shadow: 3px 6px rgba(0, 0, 0, 0.25);
+}
+
+.weather-box .weather {
+  color: #fff;
+  font-size: 48px;
+  font-weight: 700;
+  text-transform: capitalize;
+  text-shadow: 3px 6px rgba(0, 0, 0, 0.25);
+  margin-bottom: 25px;
+}
+
+.list {
+  list-style: none;
+  margin-top: 15px;
+}
+
+.list li {
+  color: #FFF;
+  font-size: 25px;
+  margin: 5px;
+}
+
 </style>
